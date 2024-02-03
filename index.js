0e8e1ebd552f68b7d9fa91b339f5425122e8488f
carolbg05 <karolbrito05@gmail.com>
2024-02-03 01:25:08 -0300

    Desafio - Entrega

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-console.log("oi")
- 
+// Defina o nome e a XP do herói
+let nomeDoHeroi = "Super Herói";
+let xpDoHeroi = 8000;
+
+// Utilize uma estrutura de decisão para determinar o nível
+let nivelDoHeroi;
+
+if (xpDoHeroi < 1000) {
+    nivelDoHeroi = "Ferro";
+} else if (xpDoHeroi <= 2000) {
+    nivelDoHeroi = "Bronze";
+} else if (xpDoHeroi <= 5000) {
+    nivelDoHeroi = "Prata";
+} else if (xpDoHeroi <= 7000) {
+    nivelDoHeroi = "Ouro";
+} else if (xpDoHeroi <= 8000) {
+    nivelDoHeroi = "Platina";
+} else if (xpDoHeroi <= 9000) {
+    nivelDoHeroi = "Ascendente";
+} else if (xpDoHeroi <= 10000) {
+    nivelDoHeroi = "Imortal";
+} else {
+    nivelDoHeroi = "Radiante";
+}
+
+// Exiba a mensagem com o nome e o nível do herói
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivelDoHeroi}`)
